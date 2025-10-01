096a76a10b69ba643bfca31f94c1efd3ce102fd5
Do Xuan Tu <doxuantutlbt@gmail.com>
2025-10-01 20:15:34 +0700

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Clients\AccountController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Clients\AuthController;
 
@@ -23,22 +24,36 @@ Route::get('/faq', function () {
     return view('clients.pages.faq');
 });
 
-// Registration Route
-Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('post-register');
+// guest routes
+Route::middleware('guest')->group(function () {
 
+    // Registration Route
+    Route::get('/register', [AuthController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('post-register');
+
+    // Login Route
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('post-login');
+
+    // Forgot Password Routes
+    Route::get('/forgot-password', [\App\Http\Controllers\Clients\ForgotPasswordController::class, 'showForgotPasswordForm'])->name('forgot-password');
+    Route::post('/forgot-password', [\App\Http\Controllers\Clients\ForgotPasswordController::class, 'sendResetLink'])->name('post-forgot-password');
+
+    Route::get('/reset-password/{token}', [\App\Http\Controllers\Clients\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/reset-password', [\App\Http\Controllers\Clients\ResetPasswordController::class, 'reset'])->name('password.update');
+});
+
+// Email Verification Route
 Route::get('/activate/{token}', [AuthController::class, 'activateAccount'])->name('activateAccount');
-
-// Login Route
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('post-login');
 
 // Logout Route
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Forgot Password Routes
-Route::get('/forgot-password', [\App\Http\Controllers\Clients\ForgotPasswordController::class, 'showForgotPasswordForm'])->name('forgot-password');
-Route::post('/forgot-password', [\App\Http\Controllers\Clients\ForgotPasswordController::class, 'sendResetLink'])->name('post-forgot-password');
+// Middleware protected routes
+Route::middleware(['auth.custom'])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/reset-password/{token}', [\App\Http\Controllers\Clients\ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('/reset-password', [\App\Http\Controllers\Clients\ResetPasswordController::class, 'reset'])->name('password.update');
+    Route::prefix('account')->group(function (){
+        Route::get('/', [AccountController::class, 'index'])->name('account.index');
+    });
+});
