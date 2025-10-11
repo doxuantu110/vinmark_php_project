f24fba6492dcb3376f0e87277d4cca2428ba342c
Arscil <23521685@gm.uit.edu.vn>
2025-10-11 19:55:44 +0700

    update file cart, file checkout

--- resources/views/clients/pages/checkout.blade.php
+++ resources/views/clients/pages/checkout.blade.php
@@ -2,30 +2,8 @@
 @extends('layouts.client')
 
 @section('title', 'Checkout')
-
+@section('breadcrumb', 'Checkout')
 @section('content')
-<!-- BREADCRUMB AREA START -->
-<div class="ltn__breadcrumb-area ltn__breadcrumb-area-2 ltn__breadcrumb-color-white bg-overlay-theme-black-90 bg-image"
-    data-bg="{{ asset('assets/clients/img/bg/9.jpg') }}">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="ltn__breadcrumb-inner ltn__breadcrumb-inner-2 justify-content-between">
-                    <div class="section-title-area ltn__section-title-2">
-                        <h6 class="section-subtitle ltn__secondary-color">// Welcome to our company</h6>
-                        <h1 class="section-title white-color">Checkout</h1>
-                    </div>
-                    <div class="ltn__breadcrumb-list">
-                        <ul>
-                            <li><a href="{{ route('home') }}">Home</a></li>
-                            <li>Checkout</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div><!-- BREADCRUMB AREA END -->
 
 <!-- CHECKOUT AREA START -->
 <div class="ltn__checkout-area mb-105">
